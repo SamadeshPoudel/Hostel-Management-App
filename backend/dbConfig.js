bcb6a52e15f6f3c2b756f9c0dca11ef2cc7ab271
Samadesh Poudel <poudelsamadesh@gmail.com>
2024-06-21 16:52:11 +0530

    Updated to .env

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
-
 async function connectDB() {
     try {
         // await mongoose.connect("mongodb+srv://poudelsamadesh:pkAV2UBUH6vQkuBS@cluster1-hostel-managem.3cxkcsp.mongodb.net/");
-        await mongoose.connect("mongodb+srv://poudelsamadesh:pkAV2UBUH6vQkuBS@cluster1-hostel-managem.3cxkcsp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1-Hostel-Management")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB connected...");
     } catch (err) {
         console.error(err.message);
